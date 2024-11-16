7dd2e1ec16b423abf1b2d9dcdad065a782d534a8
Tran Duc Huy <sandbox.duchuy02@gmail.com>
2024-11-16 22:09:50 +0700

    base folder view

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,59 @@
+<div class="sidebar-wrapper scrollbar scrollbar-inner">
+    <div class="sidebar-content">
+        <ul class="nav nav-secondary">
+            <li class="nav-item active">
+                <a
+                    data-bs-toggle="collapse"
+                    href="#dashboard"
+                    class="collapsed"
+                    aria-expanded="false"
+                >
+                    <i class="fas fa-home"></i>
+                    <p>Dashboard</p>
+                    <span class="caret"></span>
+                </a>
+                <div class="collapse" id="dashboard">
+                    <ul class="nav nav-collapse">
+                        <li>
+                            <a href="">
+                                <span class="sub-item">Dashboard 1</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-section">
+                <span class="sidebar-mini-icon">
+                  <i class="fa fa-ellipsis-h"></i>
+                </span>
+                <h4 class="text-section">Components</h4>
+            </li>
+            <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#base">
+                    <i class="fas fa-layer-group"></i>
+                    <p>Base</p>
+                    <span class="caret"></span>
+                </a>
+                <div class="collapse" id="base">
+                    <ul class="nav nav-collapse">
+                        <li>
+                            <a href="components/avatars.html">
+                                <span class="sub-item">Thêm</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components/buttons.html">
+                                <span class="sub-item">Sửa</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="components/gridsystem.html">
+                                <span class="sub-item">Xóa</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
